b374fe75ad15c61c6f9483bfe4109df4cd1d38e9
Gradius64 <66185683+Gradius64@users.noreply.github.com>
2024-08-04 20:43:47 -0500

    Add project files.

---
+++ WebApplication12/Program.cs
@@ -0,0 +1,9 @@
+GIVEN a Prework Study Guide website
+WHEN I visit the website in my browser
+THEN I see four boxes titled HTML, CSS, Git, and JavaScript with associated notes listed
+WHEN I view the study guide
+THEN I see a dark blue header and footer, and four boxes with a shadow
+WHEN I open the console in my browser
+THEN I can see the four topics I learned listed along with a suggestion on what I should study first
+WHEN I visit the website using the URL
+THEN I can access my website from any browser
